d801b569089feeae009279a6d8053b16fdda7aaa
Ramyrashad127 <ramy41599@gmail.com>
2023-10-02 11:11:22 -0700

    new

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,9 +9,9 @@ int check_cycle(listint_t *list)
 	listint_t *st;
 
 	if (!list)
-		return(0);
+		return (0);
 	st = list->next;
-	while((st != NULL) && (st != list))
+	while ((st != NULL) && (st != list))
 		st = st->next;
 	if (st == list)
 		return (1);
