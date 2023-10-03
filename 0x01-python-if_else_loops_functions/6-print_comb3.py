a0bbebf1a7efacc69fab516d07730da50e066173
Ramyrashad127 <ramy41599@gmail.com>
2023-10-03 09:27:25 -0700

    new

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(0, 10):
     for h in range(i, 10):
         if i == 8 and h == 9:
             print("{}{}".format(i, h))
-            break;
+            break
         if i == h :
             continue
         print("{}{},".format(i, h), end=' ')
