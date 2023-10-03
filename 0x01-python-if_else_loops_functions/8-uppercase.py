4de86ddf35a25f69d4606db6b7503845c37b23fc
Ramyrashad127 <ramy41599@gmail.com>
2023-10-03 10:05:42 -0700

    new

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        z = ord(str[i])
+        if z < 123 and z >= 97:
+            str[i] = chr(z - 32)
+            if i == len(str) - 1:
+                print(str[i])
+                break;
+            print(str[i], end='')
