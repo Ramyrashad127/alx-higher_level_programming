7833995a23c344c43b04994ab6e79ccacee717f5
Ramyrashad127 <ramy41599@gmail.com>
2023-10-11 11:20:48 -0700

    new

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        ma = int('-inf')
-        for i in a_dictionary:
-            if a_dictionary[i] > ma:
-                ma = a_dictionary[i]
-        return (ma)
+    if len(a_dictionary) == 0:
+        return (None)
+    ma = int('-inf')
+    for i in a_dictionary:
+        if a_dictionary[i] > ma:
+            ma = a_dictionary[i]
+    return (ma)
