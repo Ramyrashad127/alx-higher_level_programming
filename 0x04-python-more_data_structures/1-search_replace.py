58aa76721fce398950eed2abc6d3b0c1582f4399
Ramyrashad127 <ramy41599@gmail.com>
2023-10-11 06:38:24 -0700

    new

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
+    if len(my_list) != 0:
         li = []
         for i in range(len(my_list)):
             if my_list[i] == search:
