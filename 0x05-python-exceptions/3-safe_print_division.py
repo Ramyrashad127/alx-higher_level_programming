46c8ad4e3fdb328451730f551f28fd28df17b5e5
Ramyrashad127 <ramy41599@gmail.com>
2023-10-23 13:45:42 -0700

    new

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+        return res
