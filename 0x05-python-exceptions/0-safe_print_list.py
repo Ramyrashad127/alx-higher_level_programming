b26a965b6210f171be22d5bda87078168458cce1
Ramyrashad127 <ramy41599@gmail.com>
2023-10-23 12:49:16 -0700

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    ele = 0
     try:
+        ele = 0
         for i in range(x):
             print(my_list[i], end="")
             ele = ele + 1
