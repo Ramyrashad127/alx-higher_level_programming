29f301b02ebc03e358c4b200120134e3772b63b2
Ramyrashad127 <ramy41599@gmail.com>
2023-10-24 14:07:30 -0700

    new

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -39,3 +39,14 @@ class Square:
                 self.__size = value
         else:
             raise TypeError("size must be an integer")
+
+    def my_print(self):
+        """print
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
