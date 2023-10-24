7cb925077df5ed7c9336c6b9a0b19e0730f0f6b5
Ramyrashad127 <ramy41599@gmail.com>
2023-10-24 13:23:06 -0700

    new

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Square:
+    """define a square"""
+    
+    def __init__(self, size):
+        """constructor
+
+        args:
+        size: length
+        """
+        self.__size = size
