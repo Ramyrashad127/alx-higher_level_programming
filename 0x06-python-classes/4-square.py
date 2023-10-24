0c5b21c8d39e6d40ddd405de2ae77998b4a9e266
Ramyrashad127 <ramy41599@gmail.com>
2023-10-24 14:00:16 -0700

    nw

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Square:
+    """class"""
+
+    def __init__(self, size=0):
+        """constructor
+        args:
+            size: len
+        """
+        self.__size = size
+
+    def area(self):
+        """area
+        Return: area
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """property for size
+        Raises:
+            TpeError: 1
+            ValueError: 2
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter for size
+        value: new val
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
