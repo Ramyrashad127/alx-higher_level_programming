f3b71af57b9b8f99239e700dff63b4180b0e4d14
Ramyrashad127 <ramy41599@gmail.com>
2023-10-24 13:50:21 -0700

    new

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
                 self.__size = size
         else:
             raise TypeError("size must be an integer")
+
     def area(self):
         """area
         Return: area
