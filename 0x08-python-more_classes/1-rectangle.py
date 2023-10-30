3308da571b0bc5d7d109a8771171ae8215c2576c
Ramyrashad127 <ramy41599@gmail.com>
2023-10-30 02:31:55 -0700

    nw

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,16 +6,18 @@ class Rectangle:
     """ define """
     def __init__(self, width=0, height=0):
         """ initialize
-        args: 
+        args:
             width: 0
             height: 0
         """
         self.height = height
         self.width = width
+
     @property
     def width(self):
         """ return width"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """ set width"""
@@ -24,10 +26,12 @@ class Rectangle:
         if value == 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """return height"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """ set height"""
