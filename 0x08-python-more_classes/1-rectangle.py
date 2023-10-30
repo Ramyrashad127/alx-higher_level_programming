c4565f884d06c9c28ad3c01c68b87a9a5d4edf58
Ramyrashad127 <ramy41599@gmail.com>
2023-10-30 02:28:38 -0700

    new

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" module """
+
+
+class Rectangle:
+    """ define """
+    def __init__(self, width=0, height=0):
+        """ initialize
+        args: 
+            width: 0
+            height: 0
+        """
+        self.__height = height
+        self.__width = width
+    @property
+    def width(self):
+        """ return width"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """ set width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value == 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    @property
+    def height(self):
+        """return height"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """ set height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value == 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
