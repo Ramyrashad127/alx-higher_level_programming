a3305bd87856e92c86d5094b9acd28e0e63f6da8
Ramyrashad127 <ramy41599@gmail.com>
2023-11-06 10:36:11 -0800

    new

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" module """
+
+
+class BaseGeometry:
+    """ new """
+
+    def area(self):
+        """new"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ new """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ inhir """
+
+    def __init__(self, width, height):
+        """ new """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """new"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+class Square(Rectangle):
+    """ new """
+
+    def __init__(self, size):
+        """new"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
+
