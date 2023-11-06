b7b4c6dad5f7125e70bce4b7c17bc7605bd43052
Ramyrashad127 <ramy41599@gmail.com>
2023-11-06 09:21:10 -0800

    new

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,10 +2,15 @@
 """ module """
 
 
-class MyList (list):
+class MyList(list):
     """ new """
     pass
 
     def print_sorted(self):
         """ print """
+
         print(sorted(list(self)))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile('tests/1-my_list.txt')
