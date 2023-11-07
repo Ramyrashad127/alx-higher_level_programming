1a1ce09c69abe8df5dc6e2d83a5957cc5f0c2e70
Ramyrashad127 <ramy41599@gmail.com>
2023-11-07 11:38:59 -0800

    new

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Student:
+    """new"""
+    def __init__(self, first_name, last_name, age):
+        """new"""
+       self.first_name = first_name
+       self.last_name = last_name
+       self.age = age
+
+    def to_json(self):
+        """new"""
+        return self.__dict__
