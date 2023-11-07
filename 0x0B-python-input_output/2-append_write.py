437c85fc46160281e687677508ecb7508813d0b5
Ramyrashad127 <ramy41599@gmail.com>
2023-11-07 10:28:53 -0800

    new

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """module"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ a"""
     with open(filename, "a", encoding="UTF-8") as fi:
         return fi.write(text)
