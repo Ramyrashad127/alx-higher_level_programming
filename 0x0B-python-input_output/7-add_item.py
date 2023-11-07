93f3b085fd5c36af9f6294ce8481237600f5a3e3
Ramyrashad127 <ramy41599@gmail.com>
2023-11-07 11:03:17 -0800

    new

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module """
+import sys
+save = __import__('5-save_to_json_file.py').save_to_json_file
+load = __import__('6-load_from_json_file.py').load_from_json_file
+
+
+arg = list(sys.argv[1:])
+try:
+    st = load('add_item.json')
+except Exception:
+    st = []
+
+st.extend(arg)
+save(st, "add_item.json")
