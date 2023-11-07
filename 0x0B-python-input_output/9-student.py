e132e1292e062dd1253ca02d70098800608c2edb
Ramyrashad127 <ramy41599@gmail.com>
2023-11-07 11:49:32 -0800

    new

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,9 +6,10 @@ class Student:
     """new"""
     def __init__(self, first_name, last_name, age):
         """new"""
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """new"""
