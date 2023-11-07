84b30c0558ac17c2b1dcd5215de28117bbf2781a
Ramyrashad127 <ramy41599@gmail.com>
2023-11-07 11:09:22 -0800

    new

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """module """
 import sys
-save = __import__('5-save_to_json_file.py').save_to_json_file
-load = __import__('6-load_from_json_file.py').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 
 arg = list(sys.argv[1:])
