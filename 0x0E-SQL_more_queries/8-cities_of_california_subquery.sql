3a38c9caea17563e3e4e1ae5f55a49b427947907
Ramyrashad127 <ramy41599@gmail.com>
2023-12-13 09:46:39 -0800

    new

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,5 @@
 -- new task sql
-SELECT name, id FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id DESC;
+SELECT name, id FROM cities WHERE state_id = (
+	SELECT id FROM states
+	WHERE name = "California")
+ORDER BY id DESC;
