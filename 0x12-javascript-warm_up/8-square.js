5ec203aaeac75c5142a6d048956e27d38e3761f1
Ramyrashad127 <ramy41599@gmail.com>
2024-02-12 06:02:06 -0800

    new

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (!parseInt(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  let res = '';
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    res += 'x';
+  }
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(res);
+  }
+}
