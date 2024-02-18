c706c07ba5c2e286e0a5e8214aeca4a8261c158e
Ramyrashad127 <ramy41599@gmail.com>
2024-02-19 01:27:03 +0200

    new

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""import for sys and mysqldb"""
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    con = MySQLdb.connect(host='localhost',port=3306,user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3])
+    cur = con.cusor()
+    cur.execute("SELECT * FROM 'states'")
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    con.close()
+
