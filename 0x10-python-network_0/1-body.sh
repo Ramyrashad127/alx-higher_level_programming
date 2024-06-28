e6d2e3d08d31e551af7b6f88e82c42deb73dfd88
Ramyrashad127 <ramy41599@gmail.com>
2024-06-28 02:23:50 -0700

    new

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #display the body if success
-HTTP_RESPONSE=$(curl -s -w "HTTPSTATUS:%{http_code}" -o /tmp/output "$1")
-HTTP_BODY=$(cat /tmp/output)
-HTTP_STATUS=${HTTP_RESPONSE##*HTTPSTATUS:}
-if [ "$HTTP_STATUS" -eq 200 ]; then
-    echo "$HTTP_BODY"
-fi
-rm /tmp/output
+curl -sX GET $1 -L
