2c571009462392c1f10bf5fe91abb2ca742b7f85
Ramyrashad127 <ramy41599@gmail.com>
2024-07-03 04:41:18 -0700

    new

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" import modules """
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
